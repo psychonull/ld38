728a50f34bc554b0238ed539a0e579424163e17d
Pablo Novas <pjnovas@gmail.com>
2017-04-24 17:55:49 -0300

    build 4

--- assets/Menus (2)/Game Over Behavior (55)/script.ts
+++ assets/Menus (2)/Game Over Behavior (55)/script.ts
@@ -1,6 +1,11 @@
 class GameOverBehavior extends Sup.Behavior {
   awake() {
+    const stats = Game.lastStats;
     
+    const actor = Sup.getActor("GameOverStats");
+    actor.getChild("Generations").textRenderer.setText(`Generations: ${stats.generation}`);
+    actor.getChild("Population").textRenderer.setText(`Population: ${stats.population}`);
+    actor.getChild("Kills").textRenderer.setText(`Kills: ${stats.kills}`);
   }
 
   update() {
--- assets/Player (9)/PlayerCollision (47)/script.ts
+++ assets/Player (9)/PlayerCollision (47)/script.ts
@@ -23,6 +23,7 @@ class PlayerCollisionBehavior extends Sup.Behavior {
     
     aliveCells.forEach(cell => {
       if(Sup.ArcadePhysics2D.collides(this.actor.arcadeBody2D, cell.arcadeBody2D)) {
+        Game.lastStats = this.gridBehavior.getStats();
         Sup.Audio.playSound("Player/PlayerHit", 1);
         Sup.loadScene("Menus/GameOver");
       }
